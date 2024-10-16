6bb95b48bebb4345e3e6e93d06828a999f8948cb
Marcelo Dias <marcelodias.desenvolvedor@gmail.com>
2024-10-15 22:52:59 -0300

    feat: Cadastro e listagem de médicos

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -18,6 +18,7 @@ public class Medico {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+    private String telefone;
     private String email;
     private String crm;
     @Enumerated(EnumType.STRING)
@@ -25,4 +26,13 @@ public class Medico {
 
     @Embedded
     private Endereco endereco;
+
+    public Medico(DadosCadastroMedico dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.telefone = dados.telefone();
+        this.crm = dados.crm();
+        this.especialidade = dados.especialidade();
+        this.endereco = new Endereco(dados.endereco());
+    }
 }
